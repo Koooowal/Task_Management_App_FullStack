97cc8e0a8c00c97415d16e9a083f5253d3318b87
Kowal <jan.kowalski4b@o2.pl>
2025-10-07 11:11:56 +0200

    Added Base Components Structure

---
+++ task_manager/app/Components/Sidebar/Sidebar.tsx
@@ -0,0 +1,17 @@
+"use client"
+import React from 'react'
+import styled from 'styled-components'
+
+function Sidebar() {
+  return (
+    <div>
+      Sb
+    </div>
+  )
+}
+
+const SidebarStyled = styled.nav`
+
+`
+
+export default Sidebar
